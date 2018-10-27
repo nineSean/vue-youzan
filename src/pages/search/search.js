cc7ea5d1fac291da26e6027f126c073eba041589
nineSean <ninesean1989@gmail.com>
2018-10-27 09:39:33 +0800

    add 列表页回到顶部按钮显示、隐藏与过渡动画

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,11 +1,11 @@
 import 'css/common.css'
 import './search.css'
-
 import Vue from 'vue'
 import axios from 'axios'
 import qs from 'qs'
 import url from 'js/api.js'
 import mixin from 'js/mixin.js'
+import Velocity from 'velocity-animate'
 
 let {id, keyword} = qs.parse(location.search.substr(1))
 const search = new Vue({
@@ -20,6 +20,12 @@ const search = new Vue({
     this.fetch()
   },
   methods: {
+    toShowOrNot(){
+      this.isShowed = scrollY > 160 ? true : false
+    },
+    toTop(){
+      Velocity(document.body, 'scroll', {duration: 600, offset: 0})
+    },
     fetch(){
       axios.post(url.searchLists, {id, keyword}).then(res => {
         this.searchLists = res.data.lists
