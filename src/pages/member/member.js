def1a46f97ce35b93fef08b54708a93dd34c5c68
nineSean <ninesean1989@gmail.com>
2019-01-07 11:29:30 +0800

    modify 重构router配置为单独文件

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,32 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-
-Vue.use(Router)
-
-const routes = [{
-  path: '/',
-  component: require('./components/member.vue').default
-},{
-  path: '/address',
-  component: require('./components/address.vue').default,
-  children: [{
-    path: '',
-    // component: require('./components/all.vue').default
-    redirect: 'all'
-  },{
-    name: 'all',
-    path: 'all',
-    component: require('./components/all.vue').default
-  },{
-    name: 'form',
-    path: 'form',
-    component: require('./components/form.vue').default
-  }]
-}]
-const router = new Router({
-  routes
-})
-
+import router from './router'
 new Vue({
   el: '#app',
   router,
